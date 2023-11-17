2405f999177503c81b3d84a218b2705d93d102fb
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-11-17 09:29:00 -0500

    Use BeautifulSoup to scrape product price

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
+import os
+from pprint import pprint
+import lxml
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import requests
+from bs4 import BeautifulSoup
 
+user_agent = os.environ.get("User-Agent", "User Agent value not found")
+accepted_language = os.environ.get("Accepted-Language", "Accepted Language not found")
+endpoint = "https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6"
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+headers = {
+    "User-Agent": user_agent,
+    "Accepted-Language": accepted_language
+}
 
+response = requests.get(url=endpoint, headers=headers)
+soup = BeautifulSoup(response.text, "lxml")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# pprint(soup.prettify())
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+price = float(f"{soup.find(name="div", 
+                     id="corePriceDisplay_desktop_feature_div", 
+                     class_="celwidget").find(name="span", class_="a-price-whole").getText()}{soup.find(name="div", 
+                     id="corePriceDisplay_desktop_feature_div", 
+                     class_="celwidget").find(name="span", class_="a-price-fraction").getText()}")
+
+print(price)
